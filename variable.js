6bb99e7ba31206d044b127d632b46f9a1069988d
muhammadmahfuzmr <muhammadmahfuzmr@gmail.com>
2024-11-04 18:34:55 +0600

    five things to declare a variable

---
+++ variable.js
@@ -0,0 +1,23 @@
+// 5 things to declare a variable
+/*
+key word: var or let or const
+name: variable name
+equal sign: (=)
+value: variable value
+semi-colon: (;)
+
+
+*/
+var age = 22 ;
+/*
+keyword: var,
+name: age,
+equal sign: (=),
+value: 22,
+semi-colon: (;)
+
+*/
+var name = 'mahfuz';
+var money = 2522;
+console.log(money)
+console.log(age)
