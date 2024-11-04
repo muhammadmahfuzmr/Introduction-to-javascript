3a0cccf9b204cdddbf35da10a7620eb553410876
muhammadmahfuzmr <muhammadmahfuzmr@gmail.com>
2024-11-04 18:57:17 +0600

    string, number, boolean type of data

---
+++ types.js
@@ -0,0 +1,19 @@
+// string
+
+var name = 'Mahfuzur Rahman';
+var address = 'Niz Agna, Kaziganj bazar, nabiganj, habiganj';
+console.log(address)
+// boolean
+
+var isPassed = true;
+var isAdmin = false;
+console.log(isAdmin)
+// number
+
+var money = 200;
+var age = 22;
+console.log(money)
+//----------check type--------------//
+console.log(typeof money)
+console.log(typeof isPassed)
+console.log(typeof name)
